8f418a910090f8d36e387ed0d3da9490a951890d
monkymiky <michines2001@gmail.com>
2025-05-14 14:03:47 +0200

    utilizzo spring per singleton

---
+++ src/main/java/com/unipd/synclab/project1/model/User.java
@@ -0,0 +1,16 @@
+package com.unipd.synclab.project1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+}
